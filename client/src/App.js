54a8b570353683437f641031eebee6382d2c986b
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2022-01-14 14:29:08 -0600

    getting all timezone data in console

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,69 @@
-import './App.css';
-import axios from 'axios'
-import { useEffect,useState } from 'react'
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-
-const URL = 'http://worldtimeapi.org/api/ip'
+const allTimezones = "http://worldtimeapi.org/api/timezone/";
+const urlMyTime = "http://worldtimeapi.org/api/ip";
 
 function App() {
-  const [yourTime, setYourTime] = useState('')
-  const [yourAbb, setYourAbb] = useState('')
-  const [yourTimezone, setYourTimezone] = useState('')
+  const [myTime, setmyTime] = useState("");
+  const [myAbb, setmyAbb] = useState("");
+  const [myTimezone, setmyTimezone] = useState("");
+  const [allTime, setAllTime] = useState("");
 
-  
-useEffect(() => {
-  
-  async function allTime() {
-    try {
-      const res = await axios.get(URL);
-      setYourTime(res.data.datetime)
-      setYourAbb(res.data.abbreviation)
-      setYourTimezone(res.data.timezone)
-      
-      
-    } catch (error) {
-      console.log(error);
+  //get my time
+  useEffect(() => {
+    async function myTime() {
+      try {
+        const res = await axios.get(urlMyTime);
+
+        setmyTime(res.data.datetime);
+        setmyAbb(res.data.abbreviation);
+        setmyTimezone(res.data.timezone);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-  allTime()
-},[])
+    myTime();
+  }, []);
 
+  //get all timezone names
+  useEffect(() => {
+    async function allTime() {
+      try {
+        const res = await axios.get(allTimezones);
+        setAllTime(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    allTime();
+  }, []);
 
+  useEffect(() => {
+    
+    for (let i = 0; i < allTime.length; i++) {
+      let allZonesURL = allTimezones + allTime[i];
+
+      async function timeTime() {
+        try {
+          const res = await axios.get(allZonesURL);
+          let test = res.data;
+          console.log(test);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      timeTime();
+    }
+  }, []);
 
   return (
     <div className="App">
-      <div>Hello</div>
-      <div>Your time is: {yourTime.substring(11,19)}</div>
-      <div>Your timezone is: {yourAbb} | {yourTimezone}</div>
-      
+      <div>my time is: {myTime.substring(11, 19)}</div>
+      <div>
+        my timezone is: {myAbb} | {myTimezone}
+      </div>
     </div>
   );
 }
