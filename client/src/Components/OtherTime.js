263eafed59697b1bf3ecf9fbc370f6fc673796f7
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2022-01-24 22:03:14 -0600

    cors anywhere added back

--- client/src/Components/OtherTime.js
+++ client/src/Components/OtherTime.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const someKey = process.env.REACT_APP_API_KEY;
-// const corsAnywhere = "https://fivesomewheres.herokuapp.com/";
-// const URL = `${corsAnywhere}https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
-const URL = `https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
+const corsAnywhere = "https://fivesomewheres.herokuapp.com/";
+const URL = `${corsAnywhere}https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
+// const URL = `https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
 
 export default function OtherTime() {
   const [zones, setZones] = useState([]);
