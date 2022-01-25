d526a2dad5132510a863356970e3f4312ffef7e6
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2022-01-25 09:22:32 -0600

    changed key

--- client/src/Components/OtherTime.js
+++ client/src/Components/OtherTime.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const someKey = process.env.REACT_APP_API_KEY;
+// const someKey = process.env.REACT_APP_API_KEY;
+const someKey = "BOIBS6ASBS1P";
+
 const corsAnywhere = "https://fivesomewheres.herokuapp.com/";
 const URL = `${corsAnywhere}https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
 // const URL = `https://api.timezonedb.com/v2.1/list-time-zone?key=${someKey}&format=json`;
